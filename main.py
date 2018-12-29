6ff032eeae178c9be0a2f69989e7e8c26c23fcb0
鸡毛 <shihao.gu@perfma.com>
2018-12-29 14:39:15 +0800

    bp网络类

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ import sys, os;
 import os.path;
 import numpy as np;
 from mnist.mnist import mnist;
+from bpNet import bpNet;
 
-a = mnist();
+mstData = mnist();
 
-print(a.trainLabel[0]);
+net = bpNet();
 
